5a6bc2658d24d1e05c7441a3cb7050e9928ecdaa
hermanslyngstadli <herman@slyngstadli.no>
2023-01-28 16:49:36 +0100

    Fix build error

--- src/pages/LoadingRiksklubben.jsx
+++ src/pages/LoadingRiksklubben.jsx
@@ -1,5 +1,5 @@
 import { FloatTV } from '../components/FloatTV'
-import video from '../components/riksklubb-nintendo.mp4'
+import video from '../assets/riksklubb-nintendo.mp4'
 
 export const LoadingRiksklubben = () => {
     return <FloatTV videoRef={video} />

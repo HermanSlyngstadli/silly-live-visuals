2c42e2337198c8d1e3a6c04f4c8ccae55c47b442
hermanslyngstadli <herman@slyngstadli.no>
2023-01-28 16:46:22 +0100

    Video in components

--- src/pages/GlobeRiksklubben.jsx
+++ src/pages/GlobeRiksklubben.jsx
@@ -1,5 +1,5 @@
 import { FloatTV } from '../components/FloatTV'
-import video from '../assets/riksklubb-globe.mp4'
+import video from '../components/riksklubb-globe.mp4'
 
 export const GlobeRiksklubben = () => {
     return <FloatTV videoRef={video} />
